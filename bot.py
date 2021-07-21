87d0cfbbc657dc8faa017253619c9a56d6f8a76a
Zhongze Tang <i@tbis.me>
2021-07-21 02:31:18 -0400

    adding subscription management

--- bot.py
+++ bot.py
@@ -41,8 +41,7 @@ def check(update: Update, context: CallbackContext) -> int:
     logger.info("User %s starts to check time.", user.first_name)
 
     update.message.reply_text(
-        'Send /cancel to stop at any time.\n\n'
-        'What service do you want to make an appointment?\n',
+        CHECK_MSG,
         reply_markup=ReplyKeyboardMarkup(
             SERVICE_KEYBOARD, one_time_keyboard=True, input_field_placeholder='Services'
         ),
@@ -85,7 +84,6 @@ def cancel(update: Update, context: CallbackContext) -> int:
     update.message.reply_text(
         'Session ends. Bye!', reply_markup=ReplyKeyboardRemove()
     )
-
     return ConversationHandler.END
 
 
@@ -99,18 +97,15 @@ def unknown(update: Update, context: CallbackContext) -> None:
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, the command does not support.")
 
 
-def auth_check(update: Update, context: CallbackContext) -> int:
+def auth_check_subscribe(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
     logger.info("User %s starts to subscribe.", user.first_name)
     if str(user.id) not in AUTHORIZED_USERS:
-        update.message.reply_text('Sorry, you are not the authorized user, you cannot use this function.\n\n'
-                                  'Instead, you can deploy this bot by yourself.\n\n'
-                                  'Please visit ' + PRJ_URL + ' for more information.')
+        update.message.reply_text(NO_AUTH_MSG)
         return ConversationHandler.END
     else:
         update.message.reply_text(
-            'Send /cancel to stop at any time.\n\n'
-            'What service do you want to make an appointment?',
+            CHECK_MSG,
             reply_markup=ReplyKeyboardMarkup(
                 SERVICE_KEYBOARD, one_time_keyboard=True, input_field_placeholder='Services'
             ),
@@ -127,14 +122,7 @@ def location_select(update: Update, context: CallbackContext) -> int:
         return LOCATION_SELECT
 
     context.user_data['SERVICE'] = update.message.text
-    update.message.reply_text('Good. You subscribe to ' + update.message.text + '.\n\n'
-                              'Please tell me the preferred MVC location you want to subscribe. '
-                              'Please check ' + LOCATION_LIST_URL + ' for location names and ids.\n\n'
-                                                                  'Either location id or location name is '
-                                                                  'supported.\n\n '
-                                                                  'If you have no preferred MVC location and want '
-                                                                  'to subscribe to all locations, '
-                                                                  'please reply 0 or All.',
+    update.message.reply_text('Good. You subscribe to ' + update.message.text + '.\n\n' + SET_LOC_MSG,
                               reply_markup=ReplyKeyboardMarkup(
                                   [['All']], one_time_keyboard=True, input_field_placeholder='Locations'
                               ))
@@ -147,27 +135,24 @@ def time_select(update: Update, context: CallbackContext) -> int:
     location = update.message.text
     logger.info("User %s chooses %s", user.first_name, location)
 
-    if location not in LOCATION_ID.keys() and location not in LOCATION_ID.values():
-        update.message.reply_text('Sorry, location not found. Please try again.\n\n'
-                                  'Please check ' + LOCATION_LIST_URL + ' for location names and ids.\n\n'
-                                                                      'Either location id or location name is '
-                                                                      'supported.\n\n '
-                                                                      'If you have no preferred MVC location and want '
-                                                                      'to subscribe to all locations, '
-                                                                      'please reply 0 or All.',
+    service_id = SERVICE_ID[context.user_data.get('SERVICE')]
+    if location not in LOCATION_NAME[service_id].keys() and \
+            location not in LOCATION_NAME[service_id].values() and location != 'All':
+        update.message.reply_text('Sorry, location not found. Please try again.\n\n' + SET_LOC_MSG,
                                   reply_markup=ReplyKeyboardMarkup(
                                       [['All']], one_time_keyboard=True, input_field_placeholder='Locations'
                                   ))
         return TIME_SELECT
 
-    if location in LOCATION_ID.keys():
+    if location == 'All':
+        context.user_data["LOCATION_ID"] = '0'
+    elif location in LOCATION_ID.keys():
         context.user_data["LOCATION_ID"] = location
     else:
-        context.user_data["LOCATION_ID"] = [k for k, v in LOCATION_ID.items() if v == location][0]
+        context.user_data["LOCATION_ID"] = [k for k, v in LOCATION_NAME[service_id].items() if v == location][0]
 
-    update.message.reply_text('Please tell me the last date you can accept to do the business in MVC.\n\n'
-                              'The format is yymmdd (US Eastern Time), e.g., 210704 means July 4th, 2021.\n\n'
-                              'If you have no requirements on this, please reply 0 or All.',
+    update.message.reply_text('Please tell me the last date you can accept to do the business in MVC.\n\n' +
+                              TME_FMT_MSG,
                               reply_markup=ReplyKeyboardMarkup(
                                   [['All']], one_time_keyboard=True, input_field_placeholder='Time'
                               ))
@@ -181,9 +166,7 @@ def confirm_info(update: Update, context: CallbackContext) -> int:
     logger.info("User %s chooses %s", user.first_name, time)
 
     if not is_valid_date(time):
-        update.message.reply_text('Error: Wrong time format.\n\n'
-                                  'The format is yymmdd (US Eastern Time), e.g., 210707 means July 7th, 2021.\n\n'
-                                  'If you have no requirements on this, please reply 0 or All.',
+        update.message.reply_text('Error: Wrong time format.\n\n' + TME_FMT_MSG,
                                   reply_markup=ReplyKeyboardMarkup(
                                       [['All']], one_time_keyboard=True, input_field_placeholder='Time'
                                   ))
@@ -193,7 +176,7 @@ def confirm_info(update: Update, context: CallbackContext) -> int:
             time = '331231'
         context.user_data['TIME'] = time
         update.message.reply_text('You are subscribing to ' + context.user_data.get('SERVICE', 'NOT FOUND') +
-                                  ' at ' + LOCATION_ID[context.user_data.get('LOCATION_ID', '0')] +
+                                  ' at ' + LOCATION_ID[context.user_data.get('LOCATION_ID')] +
                                   ' on or before ' + context.user_data.get('TIME', '331231') + ' (yymmdd).\n\n'
                                   'Reply /confirm to start the subscription. The bot will query available '
                                   'appointments every 5 min, and send you a notification when one is available.\n\n'
@@ -207,7 +190,6 @@ def confirm_info(update: Update, context: CallbackContext) -> int:
 def appt_check(context: CallbackContext):
     job = context.job
     detail = job.context
-    print(detail['TIME'])
     if is_expired_date(detail['TIME']):
         context.bot.send_message(chat_id=detail['CHAT_ID'],
                                  text='You subscription ' + detail['name'] + ' has expired. Please start a new one.\n\n'
@@ -230,23 +212,38 @@ def appt_check(context: CallbackContext):
 def job_reg(update: Update, context: CallbackContext) -> int:
     job = {
         'CHAT_ID': update.message.chat_id,
-        'SERVICE': context.user_data.get('SERVICE', 'NOT FOUND'),
-        'LOCATION_ID': context.user_data.get('LOCATION_ID', '0'),
-        'TIME': context.user_data.get('TIME', '331231'),
+        'SERVICE': context.user_data.get('SERVICE'),
+        'LOCATION_ID': context.user_data.get('LOCATION_ID'),
+        'TIME': context.user_data.get('TIME'),
     }
 
-    if job['SERVICE'] == 'NOT FOUND':
-        update.message.reply_text('ERROR: INVALID SERVICE', reply_markup=ReplyKeyboardRemove())
-        return ConversationHandler.END
-
     job['SERVICE_URL'] = MVC_URL + SERVICE_ID[job['SERVICE']]
-    name = job['TIME'] + ', ' + LOCATION_ID[job['LOCATION_ID']] + ', ' + job['SERVICE']
+    name = job['TIME'] + ' (date in yymmdd), ' + LOCATION_ID[job['LOCATION_ID']] + ' (location), ' + job['SERVICE']
     job['NAME'] = name
     context.job_queue.run_repeating(appt_check, interval=300, first=10, name=name, context=job)
     context.user_data.clear()
     update.message.reply_text('Your subscription ' + name + ' is scheduled successfully.',
                               reply_markup=ReplyKeyboardRemove())
     return ConversationHandler.END
+
+
+def auth_check_sublist(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("User %s starts to check subscription list.", user.first_name)
+    if str(user.id) not in AUTHORIZED_USERS:
+        update.message.reply_text(NO_AUTH_MSG)
+        return ConversationHandler.END
+    else:
+        jobs = context.job_queue.jobs()
+        if len(jobs) == 0:
+            update.message.reply_text('You have no running subscriptions.')
+            return ConversationHandler.END
+        else:
+            update.message.reply_text('Here are the running subscriptions you have. Reply the index to cancel it.'
+                                      'Reply 0 to cancel all. \n\n Send /cancel to quit this procedure.')
+            for i in range(len(jobs)):
+                update.message.reply_text(str(i+1) + ': ' + jobs[i].name)
+            return 1
 
 
 def cancel_all_job(update: Update, context: CallbackContext) -> None:
@@ -270,7 +267,7 @@ def main() -> None:
     )
 
     subscribe_conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('subscribe', auth_check)],
+        entry_points=[CommandHandler('subscribe', auth_check_subscribe)],
         states={
             LOCATION_SELECT: [MessageHandler(Filters.text & (~Filters.command), location_select)],
             TIME_SELECT: [MessageHandler(Filters.text & (~Filters.command), time_select)],
@@ -280,11 +277,18 @@ def main() -> None:
         fallbacks=[CommandHandler('cancel', cancel)]
     )
 
+    sublist_conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('mysub', auth_check_sublist)],
+        states={
+
+        },
+    )
+
     dispatcher.add_handler(check_conv_handler)
     dispatcher.add_handler(subscribe_conv_handler)
+    dispatcher.add_handler(sublist_conv_handler)
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CommandHandler('help', help))
-    dispatcher.add_handler(CommandHandler('cancelall', cancel_all_job))
     dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), usr_msg))
     dispatcher.add_handler(MessageHandler(Filters.command, unknown))
 
--- utils/msg.py
+++ utils/msg.py
@@ -1,4 +1,24 @@
 
+from utils.const import *
+
+CHECK_MSG = 'Send /cancel to stop at any time.\n\n' + \
+            'What service do you want to make an appointment?\n'
+
+SET_LOC_MSG = 'Please tell me the preferred MVC location you want to subscribe. ' + \
+                 'Please check ' + LOCATION_LIST_URL + ' for location names and ids.\n\n' + \
+                 'Either location id or location name is ' + \
+                 'supported.\n\n ' + \
+                 'If you have no preferred MVC location and want ' + \
+                 'to subscribe to all locations, ' + \
+                 'please reply All.'
+
+TME_FMT_MSG = 'The format is yymmdd (US Eastern Time), e.g., 210704 means July 4th, 2021.\n\n' + \
+              'If you have no requirements on this, please reply All.'
+
+NO_AUTH_MSG = 'Sorry, you are not the authorized user, you cannot use this function.\n\n' + \
+              'Instead, you can deploy this bot by yourself.\n\n' + \
+              'Please visit ' + PRJ_URL + ' for more information.'
+
 HELP_MSG = '/check Check the most recent available places for appointment.\n' + \
            '/subscribe Receive a notification when a more recent time slot is available. Authorized users only.\n' + \
            '/help Show the help message.\n'
